1982f7fd697595611ed058b94cc185c0ef15de75
James Hibberd <james.d.hibberd@gmail.com>
2023-09-24 06:30:14 +0000

    prettier fixes to build file

--- build.js
+++ build.js
@@ -1,34 +1,34 @@
-import * as fs from 'fs'
-import * as readline from 'readline'
+import * as fs from "fs";
+import * as readline from "readline";
 
-const injectFile = function(output, tag, path) {
-    output(`<${tag}>\n`, 4)
-    fs.readFileSync(path)
-        .toString()
-        .split('\n')
-        .map(ln => output(ln + '\n', 6))
-    output(`</${tag}>\n`, 4)
-}
+const injectFile = function (output, tag, path) {
+  output(`<${tag}>\n`, 4);
+  fs.readFileSync(path)
+    .toString()
+    .split("\n")
+    .map((ln) => output(ln + "\n", 6));
+  output(`</${tag}>\n`, 4);
+};
 
-const output = function() {
-    const stream = fs.createWriteStream("build/sliding-pictre-pzuzle.html")
-    return function(content, indent) {
-        stream.write(' '.repeat(indent) + content)
-    }
-}()
+const output = (function () {
+  const stream = fs.createWriteStream("build/sliding-pictre-pzuzle.html");
+  return function (content, indent) {
+    stream.write(" ".repeat(indent) + content);
+  };
+})();
 
 const template = readline.createInterface({
-    input: fs.createReadStream('src/index.html')
-})
-template.on('line', (line) => {
-    switch (line.trim()) {
-        case '<link rel="stylesheet" href="main.css" />': 
-            injectFile(output, 'style', 'src/main.css')
-            break
-        case '<script src="main.js"></script>':
-            injectFile(output, 'script', 'src/main.js')
-            break
-        default:
-            output(line + '\n')
-    }
-})
+  input: fs.createReadStream("src/index.html"),
+});
+template.on("line", (line) => {
+  switch (line.trim()) {
+    case '<link rel="stylesheet" href="main.css" />':
+      injectFile(output, "style", "src/main.css");
+      break;
+    case '<script src="main.js"></script>':
+      injectFile(output, "script", "src/main.js");
+      break;
+    default:
+      output(line + "\n");
+  }
+});
